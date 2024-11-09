bf7c4fa0d3a89f8390ae4131093704aa4ec338e2
DenverW03 <denverwhiteley@hotmail.com>
2024-11-09 16:14:49 +0000

    canvas mouse tracking added

--- src/sand/Startup.js
+++ src/sand/Startup.js
@@ -5,9 +5,20 @@ canvas.height = 500;
 canvas.id = "canvas";
 document.body.appendChild(canvas);
 
+var mouseX;
+var mouseY;
+
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+// Handler updates the global var mouse coords
+function mouseMoveHandler(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
 var canvas = controlAddIn.appendChild(canvas);
 
-function init()
-{
+function init() {
     alert('HELLO WORLD!');
 }
