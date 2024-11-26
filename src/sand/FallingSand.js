0d6533131f8da40754dc63aeb0abc55f47d8d016
DenverW03 <denverwhiteley@hotmail.com>
2024-11-26 17:29:21 +0000

    removed comments

--- src/sand/FallingSand.js
+++ src/sand/FallingSand.js
@@ -7,9 +7,6 @@ class FallingSand {
     }
 
     changeBackgroundColor() {
-        // Should probably edit the CSS variable
-        //controlAddIn.style.backgroundColor = "orange";
-
         var root = document.querySelector(':root');
         root.style.setProperty('--bg-color', 'orange');
     }
